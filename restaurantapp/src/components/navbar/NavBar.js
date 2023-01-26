78176035319d529ea45bfe108e58c6f0c51129cb
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-26 11:54:11 -0500

    Building out the navbar component and styling the component

--- restaurantapp/src/components/navbar/NavBar.js
+++ restaurantapp/src/components/navbar/NavBar.js
@@ -1,8 +1,23 @@
 import './NavBar.css'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
+import images from '../../constants/images'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className='app-navbar'>
+      <div className='app-navbar-logo'>
+        <img src={images.gericht} alt='Gericht logo' />
+      </div>
+      <ul className='app-navbar-links'>
+        <li className='p-opensans'><a href='#home'>Home</a></li>
+        <li className='p-opensans'><a href='#about'>About</a></li>
+        <li className='p-opensans'><a href='#menu'>Menu</a></li>
+        <li className='p-opensans'><a href='#awards'>Awards</a></li>
+        <li className='p-opensans'><a href='#contact'>Contact</a></li>
+
+      </ul>
+    </nav>
   )
 }
 
