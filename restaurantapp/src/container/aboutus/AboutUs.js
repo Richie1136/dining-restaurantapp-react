d4952080d8e18b85cd09f3d407076808bfeeeb7d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-27 11:42:10 -0500

    Building out the about us component

--- restaurantapp/src/container/aboutus/AboutUs.js
+++ restaurantapp/src/container/aboutus/AboutUs.js
@@ -4,7 +4,20 @@ import { images } from '../../constants'
 const AboutUs = () => {
   return (
     <div className='app-aboutus app-bg flex-center section-padding' id='about'>
-
+      <div className='app-aboutus-overlay flex-center'>
+        <img src={images.G} alt='G letter img' />
+      </div>
+      <div className='app-aboutus-content flex-center'>
+        <div className='app-aboutus-content-about'>
+          <h1 className='headtext-cormorant'>About Us</h1>
+          <img src={images.spoon} alt='Spoon img' className='spoon-image' />
+          <p className='p-opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+          <button type='button' className='custom-button'>Know More</button>
+        </div>
+        <div className='app-aboutus-content-knife flex-center'>
+          <img src={images.knife} alt='knife img' />
+        </div>
+      </div>
     </div>
   )
 }
