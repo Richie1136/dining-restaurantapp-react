e71e9e5bb0efd90b3da1e8e8e8c39b1b5afb5590
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-30 09:17:06 -0500

    Finished the AboutUs component

--- restaurantapp/src/container/aboutus/AboutUs.js
+++ restaurantapp/src/container/aboutus/AboutUs.js
@@ -17,6 +17,12 @@ const AboutUs = () => {
         <div className='app-aboutus-content-knife flex-center'>
           <img src={images.knife} alt='knife img' />
         </div>
+        <div className='app-aboutus-content-history'>
+          <h1 className='headtext-cormorant'>Our History</h1>
+          <img src={images.spoon} alt='Spoon img' className='spoon-image' />
+          <p className='p-opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+          <button type='button' className='custom-button'>Know More</button>
+        </div>
       </div>
     </div>
   )
