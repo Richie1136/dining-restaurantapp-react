f4484a811378d47daa9562ae78386fc7ae67cd13
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-30 11:41:36 -0500

    Building out the Intro component

--- restaurantapp/src/container/intro/Intro.js
+++ restaurantapp/src/container/intro/Intro.js
@@ -1,8 +1,23 @@
 import './Intro.css'
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs'
+import { meal } from '../../constants'
+import { useRef, useState } from 'react'
 
 const Intro = () => {
+
+  const [playVideo, setPlayVideo] = useState(false)
+
+  const vidRef = useRef()
+
   return (
-    <div>Intro</div>
+    <div className='app-video'>
+      <video src={meal} ref={vidRef} type="video/mp4" loop controls="false" muted />
+      <div className='app-video-overlay flex-center'>
+        <div className='app-video-overlay-circle flex-center'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
