110bdd0430c75f40b8765030eb0a4d8136849c56
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-30 11:54:38 -0500

    Added state to be able to play and pause the video and added a tenary operator to display 2 different icons depending if i want to pause or play the video

--- restaurantapp/src/container/intro/Intro.js
+++ restaurantapp/src/container/intro/Intro.js
@@ -7,14 +7,23 @@ const Intro = () => {
 
   const [playVideo, setPlayVideo] = useState(false)
 
-  const vidRef = useRef()
+  const videoRef = useRef()
+
+  const handleVideo = () => {
+    setPlayVideo((prev) => !prev)
+
+    if (playVideo) {
+      videoRef.current.pause()
+    }
+    videoRef.current.play()
+  }
 
   return (
     <div className='app-video'>
-      <video src={meal} ref={vidRef} type="video/mp4" loop controls="false" muted />
+      <video src={meal} ref={videoRef} type="video/mp4" loop controls="false" muted />
       <div className='app-video-overlay flex-center'>
-        <div className='app-video-overlay-circle flex-center'>
-
+        <div className='app-video-overlay-circle flex-center' onClick={handleVideo}>
+          {playVideo ? <BsPauseFill color='#fff' fontSize={30} /> : <BsFillPlayFill color='#fff' fontSize={30} />}
         </div>
       </div>
     </div>
