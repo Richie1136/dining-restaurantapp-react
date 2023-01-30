b0b653a10f671bdd6c14d7918a486adf759242fa
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-30 10:53:29 -0500

    Starting to build out the chef component

--- restaurantapp/src/container/chef/Chef.js
+++ restaurantapp/src/container/chef/Chef.js
@@ -1,8 +1,14 @@
 import './Chef.css'
+import { images } from '../../constants'
+import { SubHeading } from '../../components'
 
 const Chef = () => {
   return (
-    <div>Chef</div>
+    <div className='app-bg app-wrapper section-padding'>
+      <div className='app-wrapper-img app-wrapper-img-reverse'>
+        <img src={images.chef} alt='chef' />
+      </div>
+    </div>
   )
 }
 
