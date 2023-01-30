2d6b85acc3bea466ee61b18ffc8b7938090c5d61
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-30 09:27:37 -0500

    Building out the menu component

--- restaurantapp/src/container/menu/Menu.js
+++ restaurantapp/src/container/menu/Menu.js
@@ -1,8 +1,25 @@
 import './Menu.css'
+import { images, data } from '../../constants'
+import { SubHeading, MenuItem } from '../../components'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='app-specialMenu flex-center section-padding' id='menu'>
+      <div className='app-specialMenu-title'>
+        <SubHeading title="Menu That Fits Your Palatte" />
+        <h1 className='headtext-cormorant'>Today's Special</h1>
+      </div>
+      <div className='app-specialMenu-mwnu'>
+        <div className='app-specialMenu-menu-wine flex-center'>
+          <p className='app-specialMenu-mwnu-heading'>Wine & Beer</p>
+          <div className='app-specialMenu-mwnu-items'>
+            {data.wines.map(({ title, price }) => (
+              <p>{title}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
