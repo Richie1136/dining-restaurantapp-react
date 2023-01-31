1d6759ef29a7dcfd12c60faae3251d615b078459
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-31 11:57:58 -0500

    Building out the footer component and adding styling

--- restaurantapp/src/container/footer/Footer.js
+++ restaurantapp/src/container/footer/Footer.js
@@ -1,6 +1,7 @@
 import './Footer.css'
 import { Newsletter, FooterOverlay } from '../../components'
 import { FiInstagram, FiFacebook, FiTwitter } from 'react-icons/fi'
+import { images } from '../../constants'
 
 
 const Footer = () => {
@@ -8,6 +9,34 @@ const Footer = () => {
     <div className='app-footer section-padding'>
       <FooterOverlay />
       <Newsletter />
+      <div className='app-footer-links'>
+        <div className='app-footer-links-contact'>
+          <h1 className='app-footer-headtext'>Contact Us</h1>
+          <p className='p-opensans'>9 W 53rd St, New York, NY 10019, USA</p>
+          <p className='p-opensans'>+1 212-344-1230</p>
+          <p className='p-opensans'>+1 212-555-1230</p>
+        </div>
+        <div className='app-footer-links-logo'>
+          <img src={images.gericht} alt='footer-logo' />
+          <p className='p-opensans'>The best way to find yourself is to lose yourself in the service of others</p>
+          <img className='spoon-img' style={{ marginTop: 15 }} src={images.spoon} alt='spoon img' />
+          <div className='app-footer-links-icons'>
+            <FiFacebook />
+            <FiTwitter />
+            <FiInstagram />
+          </div>
+        </div>
+        <div className='app-footer-links-work'>
+          <h1 className='app-footer-headtext'>Working Hours</h1>
+          <p className='p-opensans'>Monday-Friday:</p>
+          <p className='p-opensans'>08:00 am -12:00 am</p>
+          <p className='p-opensans'>Saturday-Sunday:</p>
+          <p className='p-opensans'>07:00am -11:00 pm</p>
+        </div>
+      </div>
+      <div className='footer-copyright'>
+        <p className='p-opensans'>2023 Gericht. All Rights Reserved.</p>
+      </div>
     </div>
   )
 }
